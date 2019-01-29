727273be6061430fe8ba8d64856a0fc9f0e97d44
Evgeniy Myatlev <33958996+myatlevEvg@users.noreply.github.com>
2019-01-29 17:32:17 +0400

    Update cuba-cli.rb
    cuba-cli v2.0.3

--- Formula/cuba-cli.rb
+++ Formula/cuba-cli.rb
@@ -2,7 +2,7 @@ class CubaCli < Formula
   desc "CUBA Platform Development Command-Line Interface "
   homepage "https://www.cuba-platform.com/"
   url "https://cuba-platform.bintray.com/tools/cuba-cli/2.0.2/cuba-cli-2.0.2-macos.zip"
-  sha256 "b12734b9d1dae6a2366ae4c2c3c66c335eef776d8cd2da267ce5188c39a23bb7"
+  sha256 "f8faf2461365d00a012306cf62938b2352e84ea06ee21d2839fe95ffe0fa5730 "
 
   def install
     prefix.install Dir["*"]
