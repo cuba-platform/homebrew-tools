8efc2b7c0bc9fbf330caebf96dd366bb7c9666df
Evgeniy Myatlev <33958996+myatlevEvg@users.noreply.github.com>
2019-02-22 16:50:08 +0400

    cuba-cli v2.0.4

--- Formula/cuba-cli.rb
+++ Formula/cuba-cli.rb
@@ -1,8 +1,8 @@
 class CubaCli < Formula
   desc "CUBA Platform Development Command-Line Interface "
   homepage "https://www.cuba-platform.com/"
-  url "https://cuba-platform.bintray.com/tools/cuba-cli/2.0.3/cuba-cli-2.0.3-macos.zip"
-  sha256 "f8faf2461365d00a012306cf62938b2352e84ea06ee21d2839fe95ffe0fa5730"
+  url "https://cuba-platform.bintray.com/tools/cuba-cli/2.0.4/cuba-cli-2.0.4-macos.zip"
+  sha256 "1b0962c311cb64cc4e707936a5d9a45134948157809a894e5b5890cfd54db0bf"
 
   def install
     prefix.install Dir["*"]
