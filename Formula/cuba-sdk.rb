82af3248e9dee93cbc8d897ee011e0d39ef2e0c5
Teamcity Integration <github@haulmont.com>
2020-09-15 17:41:08 +0400

    CUBA SDK v1.0.4

--- Formula/cuba-sdk.rb
+++ Formula/cuba-sdk.rb
@@ -2,8 +2,8 @@
 class CubaSdk < Formula
   desc "CUBA SDK is a command-line tool that provides an ability to resolve, export and upload to external repository all dependencies for CUBA framework, add-ons or any external library with few simple commands."
   homepage "https://www.cuba-platform.com/"
-  url "https://cuba-platform.bintray.com/tools/cuba-sdk/1.0.3/cuba-sdk-1.0.3-macos.zip"
-  sha256 "1d8845759cd3e3388d65a5f4becdb0218f6c79ffa39aa91adeac1e6dd2de1df2"
+  url "https://cuba-platform.bintray.com/tools/cuba-sdk/1.0.4/cuba-sdk-1.0.4-macos.zip"
+  sha256 "080ec1b2a70cef1fe1a47345fead239be56e29e3720ebfaf4f00e16ef0156a4c"
 
   def install
     prefix.install Dir["*"]
